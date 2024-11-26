f8f7ed7ef6bce44c347acf5da4631d6416a795a7
merkandez <131488754+merkandez@users.noreply.github.com>
2024-11-26 18:56:09 +0100

    feat: setting up models

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,34 +1,38 @@
 import connectionDb from '../database/connectionDb'
 import { DataTypes } from 'sequelize'
 
-const adminModel = connectionDb.define(
-    'Admin',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        rol: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    },
-    {
-        sequelize: connectionDb,
-        modelName: 'Admin',
-        tableName: 'admins',
-        timestamps: false,
-    }
-)
 
-export default adminModel
+
+    const AdminModel = connectionDb.define('Admin', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      rol: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      tableName: 'admins',
+      timestamps: false
+    });
+  
+    AdminModel.associate = (models) => {
+      AdminModel.hasMany(models.Enrollment, {
+        foreignKey: 'id_admin',
+        as: 'enrollments'
+      });
+    };
+  
+
+  export default Admin;
