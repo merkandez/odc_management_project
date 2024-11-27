89a9f8fd8c1017414b1d062d982d765564217985
jennyfer85 <jmtg05@gmail.com>
2024-11-27 15:56:06 +0100

    feat: fix config

--- server/config.js
+++ server/config.js
@@ -2,19 +2,6 @@ import { config } from 'dotenv'
 
 config()
 
-<<<<<<< HEAD
-export const DB_PASSWORD =process.env.DB_PASSWORD;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_USER =process.env.DB_USER;
-export const DB_DEV_NAME = process.env.DB_DEV_NAME;
-export const DB_PORT = process.env.DB_PORT;
-export const PORT = process.env.PORT;
-export const DB_TEST_NAME =process.env.DB_TEST_NAME;
-export const NODE_ENV = process.env.NODE_ENV;
-export const JWT_SECRET =process.env.JWT_SECRET;
-
- console.log(PORT);
-=======
 export const DB_PASSWORD = process.env.DB_PASSWORD
 export const DB_HOST = process.env.DB_HOST
 export const DB_USER = process.env.DB_USER
@@ -26,4 +13,3 @@ export const NODE_ENV = process.env.NODE_ENV
 export const JWT_SECRET = process.env.JWT_SECRET
 
 console.log(PORT)
->>>>>>> f68a9ea0805d8dc12ad700b8503b4b1dec95a0fe
