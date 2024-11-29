315814c0c3c7e6d06b8c618589f35d18a1cd106a
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-11-29 17:07:39 +0100

    feat: CREATE ENROLLMENT INCLUDING MINORS implemented

--- server/controllers/enrollmentsController.js
+++ server/controllers/enrollmentsController.js
@@ -104,77 +104,37 @@ export const createEnrollment = async (req, res) => {
 };
 
 // UPDATE ENROLLMENT BY ID
+
 export const updateEnrollmentById = async (req, res) => {
     try {
         const { id } = req.params;
-        const { fullname, email, gender, age, is_first_activity, id_admin, id_course, group_id, accepts_newsletter, minors, createdAt, updatedAt } = req.body;
-
-        const enrollment = await Enrollment.findByPk(id,
-            {transaction}
-        );
-
+        const { fullname, email, gender, age, is_first_activity, id_admin, id_course, group_id, accepts_newsletter, createdAt, updatedAt, minors } = req.body;
+        const enrollment = await Enrollment.update(
+            {
+                fullname,
+                email,
+                gender,
+                age,
+                is_first_activity,
+                id_admin,
+                id_course,
+                group_id,
+                accepts_newsletter,
+                createdAt,
+                updatedAt
+            },
+            {
+                where: { id }
+            });
         if (!enrollment) {
-            await transaction.rollback();
             return res.status(404).json({ message: "Enrollment not found" });
         }
-
-        await Enrollment.update({
-            fullname,
-            email,
-            gender,
-            age,
-            is_first_activity,
-            id_admin,
-            id_course,
-            group_id,
-            accepts_newsletter,
-            createdAt,
-            updatedAt
-        },
-    {
-            where: { id },
-            transaction
-        });
-        if (minors && minors.length > 0) {
-for(const minor of minors) {
-    if(minor.id) {
-        await Minor.update({
-            name: minor.name,
-            age: minor.age,
-        },
-        {
-            where: { id: minor.id },
-            transaction
-        });
-    } else {
-        await Minor.create({
-            name: minor.name,
-            age: minor.age,
-            enrollment_id: id,
-        },
-        {
-            transaction
-        });
-    }
-}
-        }
-        await transaction.commit();
-        const updatedEnrollment = await Enrollment.findOne({
-            where: { id },
-            include: [
-                {
-                    model: Minor,
-                    as: 'minors',
-                    attributes: ['id', 'name', 'age' ],
-                },
-            ],
-        });
-        res.status(200).json(updatedEnrollment);
+        res.status(200).json(enrollment);
     } catch (error) {
-        await transaction.rollback();
         res.status(500).json({ message: error.message });
     }
-};
+};  
+
 
 // DELETE ENROLLMENT BY ID
 export const deleteEnrollmentById = async (req, res) => {
