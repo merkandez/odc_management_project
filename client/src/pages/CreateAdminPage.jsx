d2d0d2523ed9a7e2dae4c81d9f136b058fe48750
Sarixav <sarixfridix@gmail.com>
2024-12-01 00:45:44 +0100

    modification: change of location in new-admin form

--- client/src/pages/CreateAdminPage.jsx
+++ client/src/pages/CreateAdminPage.jsx
@@ -5,17 +5,19 @@ const CreateAdminPage = () => {
   return (
     <div className="flex min-h-screen">
 
-      {/* Formulario acceso derecha */}
-      <div className="w-1/2 bg-white p-8">
-
-        <CreateAdminForm />
-      </div>
+    
 
       {/* Imagen izquierda */}
 
       <div className="w-1/2 bg-cover bg-center flex justify-center mb-10">
         <img src="/src/assets/orange-form.gif" alt="Formulario en movimiento" />
       </div>
+
+        {/* Formulario acceso derecha */}
+        <div className="w-1/2 bg-white p-8">
+
+<CreateAdminForm />
+</div>
 
 
 
