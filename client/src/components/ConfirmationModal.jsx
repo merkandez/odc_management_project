44e417b1347c42a3157499189f6b6c051c17165d
merkandez <131488754+merkandez@users.noreply.github.com>
2024-11-30 20:54:29 +0100

    fix: ConfirmationModal styles

--- client/src/components/ConfirmationModal.jsx
+++ client/src/components/ConfirmationModal.jsx
@@ -21,38 +21,38 @@ const ConfirmationModal = ({
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'
       onClick={onClose}
     >
       <div
-        className="bg-white border-2 border-gray-300  max-w-md w-full p-6 relative shadow-md"
+        className='bg-white border-2 border-gray-300 w-full max-w-md p-6 relative shadow-md sm:max-w-sm sm:px-4 '
         onClick={(e) => e.stopPropagation()}
       >
         {/* Botón de cierre */}
         <button
-          className="absolute top-2 right-2 text-black font-black text-base"
+          className='absolute top-2 right-2 text-black font-black text-base'
           onClick={onClose}
         >
           ✕
         </button>
 
         {/* Título del modal */}
-        <h3 className="text-lg font-bold mb-4 text-black">Modal title</h3>
+        <h3 className='text-lg font-bold mb-4 text-black'>Modal title</h3>
 
         {/* Mensaje */}
-        <p className="text-gray-700 mb-6">{message}</p>
+        <p className='text-gray-700 mb-6'>{message}</p>
 
         {/* Botones */}
         {showButtons && (
-          <div className="flex justify-end gap-4">
+          <div className='flex justify-end gap-4'>
             <button
-              className="border-2 border-black text-black px-4 py-2 font-bold hover:bg-gray-100"
+              className='border-2 border-black text-black px-4 py-2 font-bold hover:bg-gray-100'
               onClick={onClose}
             >
               Cancel
             </button>
             <button
-              className="bg-primary text-black px-4 py-2 font-bold  hover:bg-orange-600"
+              className='bg-primary text-black px-4 py-2 font-bold hover:bg-orange-600'
               onClick={onConfirm}
             >
               Confirm
