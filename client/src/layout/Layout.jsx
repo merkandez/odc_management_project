50bba47e99143fe1419e64e8420fbc0ad39db08a
merkandez <131488754+merkandez@users.noreply.github.com>
2024-11-30 20:11:23 +0100

    feat: add TesModal to test ConfirmationModal styles and functionality

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,18 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import Nav from '../components/Nav/Nav'
-import Footer from '../components/Footer'
-
+import { Outlet } from 'react-router-dom';
+import Nav from '../components/Nav/Nav';
+import Footer from '../components/Footer';
+import TestModal from '../components/TestModal'; // Importa el componente de prueba
 
 const Layout = () => {
-    return (
-        <div>
-            <Nav />
-           
+  return (
+    <div>
+      <Nav />
 
-            <Outlet className="h-[100vh] bg-blue-500" />
-            <Footer />
-        </div>
-    )
-}
+      {/* TestModal: Modal para pruebas */}
+      <TestModal />
+      <Outlet className='h-[100vh] bg-blue-500' />
+      <Footer />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
