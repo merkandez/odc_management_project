ddf665c6f21bb30e638a55d02c2f6424c53d6f27
jennyfer85 <jmtg05@gmail.com>
2024-12-02 17:31:33 +0100

    feat: add DashboardPage

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,23 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Nav from '../components/Nav/Nav'
 import Footer from '../components/Footer'
-import Sidebar from '../components/Sidebar';     
-import AdminPanel from '../components/AdminPanel'         
 
 const Layout = () => {
     return (
         <div>
-      <Nav />
-      <div className="flex flex-col md:flex-row">
-        {/*Barra lateral */}
-        <Sidebar className="bg-black text-white p-4 md:w-1/4" />
-        <div className="flex-1 bg-gray-100 p-6">
-          <Outlet />
-          <AdminPanel />
+            <Nav />
+            <Outlet />
+            <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  )
+    )
 }
+
 export default Layout;
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../layout/Layout'
 import FormPage from '../pages/FormPage'
 import { createBrowserRouter } from 'react-router-dom'
+import DashboardPage from '../pages/DashboardPage'
 
 
 
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <FormPage />,
+            },
+            {
+                path: 'dashboard',
+                element: <DashboardPage/>,
             },
             
         ],
