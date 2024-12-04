5ffbe648483c21ba8b0dc0e9c7ed332d03d188f8
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-12-04 04:02:07 +0100

    fix:registration and login settings

--- client/src/components/CreateAdminForm.jsx
+++ client/src/components/CreateAdminForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import SubmitButton from './SubmitButton'; 
 
 const CreateAdminForm = () => {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
@@ -23,13 +23,13 @@ const CreateAdminForm = () => {
 
     //Solicitud al back//
     try {
-      const response = await fetch('http://localhost:3000/api/new-admin', {
+      const response = await fetch('http://localhost:3000/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          email,
+          username,
           password,
           role_id: 1,  
         }),
@@ -57,17 +57,17 @@ const CreateAdminForm = () => {
 
         <form onSubmit={handleSubmit} className="space-y-6">
 
-          {/* Email */}
+          {/* Usuario */}
           <div className="flex flex-col">
-            <label htmlFor="email" className="block text-sm font-semibold text-gray-600">Email</label>
+            <label htmlFor="username" className="block text-sm font-semibold text-gray-600">Usuario</label>
             <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               required
               className="w-full p-3 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
-              placeholder="Correo electrónico"
+              placeholder="Nombre de usuario"
             />
           </div>
 
