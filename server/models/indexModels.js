e6d300d5a522db5253c3bf9b6433f0bc21b12cc7
merkandez <131488754+merkandez@users.noreply.github.com>
2024-12-08 17:37:14 +0100

    fix: setting up in indexModels and adjustment in bbddto fix the functionallity of the enrollment form

--- server/models/indexModels.js
+++ server/models/indexModels.js
@@ -6,29 +6,29 @@ import Minor from './minorModel.js';
 import connectionDb from '../database/connectionDb.js';
 
 // Roles y Admins
-Role.hasMany(Admin, { foreignKey: 'role_id', as: 'admins' })
-Admin.belongsTo(Role, { foreignKey: 'role_id', as: 'role' })
+Role.hasMany(Admin, { foreignKey: 'role_id', as: 'admins' });
+Admin.belongsTo(Role, { foreignKey: 'role_id', as: 'role' });
 
 // Enrollments y Courses
-Course.hasMany(Enrollment, { foreignKey: 'id_course', as: 'enrollments' })
-Enrollment.belongsTo(Course, { foreignKey: 'id_course', as: 'course' })
+Course.hasMany(Enrollment, { foreignKey: 'id_course', as: 'enrollments' });
+Enrollment.belongsTo(Course, { foreignKey: 'id_course', as: 'course' });
 
 // Enrollments y Admins
-Admin.hasMany(Enrollment, { foreignKey: 'id_admin', as: 'enrollments' })
-Enrollment.belongsTo(Admin, { foreignKey: 'id_admin', as: 'admin' })
+Admin.hasMany(Enrollment, { foreignKey: 'id_admin', as: 'enrollments' });
+Enrollment.belongsTo(Admin, { foreignKey: 'id_admin', as: 'admin' });
 
 // Enrollments y Minors
-Enrollment.hasMany(Minor, { foreignKey: 'enrollment_id', as: 'minors' })
-Minor.belongsTo(Enrollment, { foreignKey: 'enrollment_id', as: 'enrollment' })
+Enrollment.hasMany(Minor, { foreignKey: 'enrollment_id', as: 'minors' });
+Minor.belongsTo(Enrollment, { foreignKey: 'enrollment_id', as: 'enrollment' });
 
 // Exportar modelos y función de sincronización
 const syncModels = async () => {
   try {
-    await connectionDb.sync({ force: true }); // Actualiza la base de datos según los modelos
+    await connectionDb.sync({ alter: false });
     console.log('Modelos sincronizados con la base de datos (^_-)db(-_^) 🚀');
   } catch (error) {
     console.error('Error al sincronizar modelos:', error.message);
   }
 };
 
-export { Role, Admin, Minor, Enrollment, Course, syncModels }
+export { Role, Admin, Minor, Enrollment, Course, syncModels };
