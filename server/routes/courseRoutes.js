156841ea3a9c0795d237e49fa86e36db481096ed
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-12-09 14:51:54 +0100

    merge dev

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -5,8 +5,8 @@ import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const courseRoutes = express.Router();
 
-courseRoutes.get('/', authMiddleware, checkRol(['superadmin', 'admin', 'facilitator']), getAllCourses);
-courseRoutes.get('/:id', authMiddleware, checkRol(['superadmin', 'admin', 'facilitator']), getCourseById);
+courseRoutes.get('/',  getAllCourses);
+courseRoutes.get('/:id',  getCourseById);
 courseRoutes.get('/:id/students', authMiddleware, checkRol(['superadmin', 'admin', 'facilitator']), getStudentsByCourse);
 courseRoutes.post('/', authMiddleware, checkRol(['superadmin', 'admin']), createCourse);
 
