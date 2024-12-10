d89acf666a07e3e98f3b5cecc9f80bd9cbc61d3f
jennyfer85 <jmtg05@gmail.com>
2024-12-10 11:00:19 +0100

    style: add styles fakepage

--- client/src/pages/CoursesPage.jsx
+++ client/src/pages/CoursesPage.jsx
@@ -29,8 +29,8 @@ const CoursesPage = () => {
   if (error) return <p className="text-red-500">{error}</p>;
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-2xl font-bold text-center mb-6">Cursos Disponibles</h1>
+    <div className="flex flex-col items-center mb-8">
+      <h1 className="text-3xl font-bold text-center mb-8">Cursos Disponibles</h1>
       <div className="grid gap-4 justify-center w-full"
           style={{
             display: 'grid',
@@ -42,17 +42,19 @@ const CoursesPage = () => {
         {courses.map((course) => (
           <div
             key={course.id}
-            className="border p-4 rounded shadow-md w-80 text-center"
+            className="border p-4 rounded shadow-md w-80"
           >
             <h2 className="font-semibold text-lg">{course.title}</h2>
             <p className="text-gray-600">{course.description}</p>
+            <div className='mt-4 text-left'>
             <button
-              className="mt-4 bg-orange-500 text-dark px-4 py-2 rounded"
+              className="mt-4 bg-orange text-dark px-4 py-2 rounded-none hover:bg-black hover:text-white transition-colors"
               onClick={() => navigate(`/inscription/${course.id}`)}
             >
               Inscribirme
             </button>
           </div>
+        </div>
         ))}
       </div>
     </div>
